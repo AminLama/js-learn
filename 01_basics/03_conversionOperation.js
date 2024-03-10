0029309bb618524e649065ef4dc15a877758142c
Amin Lama <amin.lama@thamescollege.edu.np>
2024-03-10 14:55:42 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let point = "55"
+
+console.log(typeof point);
+
+let valueInNumber = Number(point)
+console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
