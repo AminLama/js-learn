99d7b1be443ae1c7ca505a203f2438b2892b6e6a
Amin Lama <amin.lama@thamescollege.edu.np>
2024-03-10 14:01:48 +0000

    datatypes basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "Amin" //string
+let age = 29 //number
+let isLoggedIn = false //boolean
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
